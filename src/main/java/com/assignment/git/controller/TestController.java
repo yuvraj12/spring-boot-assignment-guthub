7759f3673681956855f1bfbc91ecde0fd74eaee0
yuvraj12 <yuvraj.vish@gmail.com>
2022-01-30 19:03:53 +0530

    final change pushed to the repo
    final change pushed to the repo

--- src/main/java/com/assignment/git/controller/TestController.java
+++ src/main/java/com/assignment/git/controller/TestController.java
@@ -3,6 +3,7 @@ package com.assignment.git.controller;
 import java.util.List;
 import java.io.IOException;
 import java.util.ArrayList;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,57 +12,76 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.core.JsonProcessingException;
-//import com.fasterxml.jackson.databind.JsonNode;
-//import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 public class TestController {
-	
+
 	public static String BASE_URL = "https://api.github.com";
 	public static String LIST_REPO_API = "/orgs/vmware/repos";
 	public static String REPO_DETAILS_API = "/repos/vmware/clarity";
 	public static String COMMIT_REPO_API = "/repos/yuvraj12/repo1/commits";
+	public static String ERROR_RESPONSE_API ="\"{\"HTTP STATUS\": \"NOT OK\",\"HTTP CODE\": \"UNDEFIND\",\"BODY\": \"[EMPTY RESPONSE PLEASE CHECK THE SERVER]\"}\"";
 
-    @RequestMapping(value = "/listrepodetails", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public List<String> getList() throws JsonProcessingException, IOException  {
-    	RestTemplate rest = new RestTemplate();
-    	ResponseEntity<String> response = rest.getForEntity(BASE_URL+LIST_REPO_API, String.class);
-    	
-//    	ObjectMapper mapper = new ObjectMapper();
-//    	JsonNode jsonnode = mapper.readTree(response.getBody().toString());
+	
+	/**
+	 * This getList method returns the list of repositories from given inputs
+	 * 
+	 * @throws JsonProcessingException
+	 * @throws IOException
+	 */
+	@RequestMapping(value = "/listrepodetails", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody 
+	public List<String> getList() throws JsonProcessingException, IOException {
+		RestTemplate rest = new RestTemplate();
+		ResponseEntity<String> response = rest.getForEntity(BASE_URL + LIST_REPO_API, String.class);
+		List<String> list = new ArrayList<String>();
+		if (HttpStatus.OK != null) {
+			list.add(response.getBody());
+		} else {
+			list.add(ERROR_RESPONSE_API);
+		}
+		return list;
 
-//    	JsonNode name = jsonnode.path("name");
-    	
-    	List<String> list = new ArrayList<String>();
-    	list.add(response.getBody());
-//    	assertThat(response.getStatusCode(), equals(HttpStatus.OK));    	
-//    	System.out.println("list ===== "+response.getBody());
-//    	System.out.println("name ===== "+name);
+	}
+	
+	
 
-//    	assertThat(response.getStatusCode(), equals(HttpStatus.OK));
-    	System.out.println("||||||| List of Repositories ||||||||||");
-    	return list;
-    }
-    
-    
+	/**
+	 * This getList method returns the list of repositories from given inputs
+	 * 
+	 * @return String object in the form of JSON
+	 * @throws JsonProcessingException
+	 * @throws IOException
+	 */
+	@RequestMapping(value = "/repodetails", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String repodetails() throws JsonProcessingException, IOException {
+		RestTemplate rest = new RestTemplate();
+		ResponseEntity<String> response = rest.getForEntity(BASE_URL + REPO_DETAILS_API, String.class);
+		if (HttpStatus.OK != null) {
+			return response.getBody();
+		} else {
+			return ERROR_RESPONSE_API;
+		}
+	}
 
-    @RequestMapping(value = "/repodetails", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public String repodetails() throws JsonProcessingException, IOException  {
-    	RestTemplate rest = new RestTemplate();
-    	ResponseEntity<String> response = rest.getForEntity(BASE_URL+REPO_DETAILS_API, String.class);
-    	System.out.println("||||||| Repostory Details ||||||||||");
-    	return response.getBody();
-    }
-    
-    @RequestMapping(value = "/commitdetails", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public String  commitdetails() throws JsonProcessingException, IOException  {
-    	RestTemplate rest = new RestTemplate();
-    	ResponseEntity<String> response = rest.getForEntity(BASE_URL+COMMIT_REPO_API, String.class);
-    	System.out.println("||||||| Commit Details ||||||||||");
-    	return response.getBody();
-    }
-    
+	/**
+	 * This getList method returns the list of repositories from given inputs
+	 * 
+	 * @return String object in the form of JSON
+	 * @throws JsonProcessingException
+	 * @throws IOException
+	 */
+	@RequestMapping(value = "/commitdetails", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String commitdetails() throws JsonProcessingException, IOException {
+		RestTemplate rest = new RestTemplate();
+		ResponseEntity<String> response = rest.getForEntity(BASE_URL + COMMIT_REPO_API, String.class);
+		if (HttpStatus.OK != null) {
+			return response.getBody();
+		} else {
+			System.out.println("in else");
+			return ERROR_RESPONSE_API;
+		}
+	}
 }
--- src/test/java/com/assignment/git/controller/TestControllerTest.java
+++ src/test/java/com/assignment/git/controller/TestControllerTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cache.CacheManager;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,17 +19,25 @@ import static org.hamcrest.Matchers.*;
 @RunWith(SpringRunner.class)
 @WebMvcTest(TestController.class)
 public class TestControllerTest {
-	
+	public static String REPO_RECORD_1 ="REPO_RECORD_1";
+	public static String COMMIT_RECORD ="Commit record";
+
 	@Autowired
 	private MockMvc mockMvc;
 	
 	@MockBean
 	private TestController testcontroller;
-	
-	
+		
+	@Autowired private CacheManager cacheManager; 
+
+	public void evictAllCaches(){ 
+	    for(String name : cacheManager.getCacheNames()){
+	        cacheManager.getCache(name).clear(); 
+	    } 
+	}
 	@Test
 	public void getlistRepoTest() throws Exception {
-	    List<String> records = new ArrayList<String>();//(Arrays.asList("RECORD_1", "RECORD_2", "RECORD_3")); 
+	    List<String> records = new ArrayList<String>();
 	    records.add("RECORD_1");
 	    records.add("RECORD_2");
 	    records.add("RECORD_3");
@@ -39,25 +48,21 @@ public class TestControllerTest {
 	    .contentType(MediaType.APPLICATION_JSON))
 		.andExpect(status().isOk())
 		.andExpect(jsonPath("$", hasSize(3)));
-//		.andExpect(jsonPath("$[0]", is("RECORD_1")));
 	}
 
 	@Test
 	public void getRepoDetailsTest() throws Exception {
-	    String records  = "RECORD_1";
-	    Mockito.when(testcontroller.repodetails()).thenReturn(records);
+	    Mockito.when(testcontroller.repodetails()).thenReturn(REPO_RECORD_1);
 	    this.mockMvc.perform(MockMvcRequestBuilders.get("/repodetails"))
         .andExpect(status().isOk())
-        .andExpect(content().string(containsString("RECORD_1")));
+        .andExpect(content().string(containsString(REPO_RECORD_1)));
 	}
 	
 	@Test
 	public void getCoomitDetailsTest() throws Exception {
-	    String records  = "Commit record";
-	    records = "Commit Record2";
-	    Mockito.when(testcontroller.commitdetails()).thenReturn(records);
+	    Mockito.when(testcontroller.commitdetails()).thenReturn(COMMIT_RECORD);
 	    this.mockMvc.perform(MockMvcRequestBuilders.get("/commitdetails"))
         .andExpect(status().isOk())
-        .andExpect(content().string(containsString("Commit Record2")));
+        .andExpect(content().string(containsString(COMMIT_RECORD)));
 	}
 }
